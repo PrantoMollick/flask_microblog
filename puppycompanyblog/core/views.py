62480d3188501d275ae0598f92c4a51435292a94
Pranto Mollick <prantocse01@gmail.com>
2021-07-04 09:06:58 -0400

    basic blog website done by flask jinja2 bootstarap5

--- puppycompanyblog/core/views.py
+++ puppycompanyblog/core/views.py
@@ -1,12 +1,16 @@
 # core/views.py
 from flask import render_template, request, Blueprint
+from puppycompanyblog.models import BlogPost
 
 core = Blueprint('core', __name__)
 
 @core.route('/')
 def index():
     # More come
-    return render_template('index.html'), 200
+    page = request.args.get('page', 1, type=int)
+    blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page, per_page=5)
+    print(blog_posts.__dict__)
+    return render_template('index.html', blog_posts=blog_posts), 200
 
 @core.route('/info')
 def info():
